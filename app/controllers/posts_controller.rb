3bce1b1d9b723b6ccae68b398fdf82a83dc77043
Camille <camilleseguette@gmail.com>
2016-07-30 22:41:08 -0500

    Assignment 31 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,19 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+    def index
+      @posts = Post.all
+      @posts.each_with_index do |post, index|
+        if index % 5 == 0
+          post.title = "SPAM"
+        end
+      end
 
-  def show
-  end
+    def show
+    end
 
-  def new
-  end
+    def new
+    end
 
-  def edit
+    def edit
+    end
   end
 end
