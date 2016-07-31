d990f4f69cbfcb9413775a75ee117eddd94398e6
Camille <camilleseguette@gmail.com>
2016-07-30 19:54:44 -0500

    Assignment 30 completed with faults

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,7 @@
 
 require 'random_data'
 
-# Create Posts
 50.times do
-
   Post.create!(
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -18,14 +16,24 @@ require 'random_data'
 end
 posts = Post.all
 
-# Create Comments
-
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+puts "#{Post.count}"
+Post.find_or_create_by(
+  title: "Techno Interests Me",
+  body: "Suffer the rave"
+)
+puts "#{Post.count}"
+
+Comment.find_or_create_by(
+  post: posts.find_or_create_by
+  body: "Suffer the rave"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
